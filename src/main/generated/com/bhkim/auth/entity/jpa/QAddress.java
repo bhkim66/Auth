7a0d186577fdbab3f96666f18c946d0de0b5577e
bhkim66 <bhmklnj63@gmail.com>
2024-12-16 22:52:04 +0900

    yml 설정 변경 jenkins

---
+++ src/main/generated/com/bhkim/auth/entity/jpa/QAddress.java
@@ -0,0 +1,59 @@
+package com.bhkim.auth.entity.jpa;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QAddress is a Querydsl query type for Address
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAddress extends EntityPathBase<Address> {
+
+    private static final long serialVersionUID = 1114930026L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QAddress address = new QAddress("address");
+
+    public final StringPath address1 = createString("address1");
+
+    public final StringPath address2 = createString("address2");
+
+    public final NumberPath<Long> addressSeq = createNumber("addressSeq", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final QUser user;
+
+    public final NumberPath<Integer> zipcode = createNumber("zipcode", Integer.class);
+
+    public QAddress(String variable) {
+        this(Address.class, forVariable(variable), INITS);
+    }
+
+    public QAddress(Path<? extends Address> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QAddress(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QAddress(PathMetadata metadata, PathInits inits) {
+        this(Address.class, metadata, inits);
+    }
+
+    public QAddress(Class<? extends Address> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
+    }
+
+}
+
---
+++ src/main/generated/com/bhkim/auth/entity/jpa/QUser.java
@@ -0,0 +1,53 @@
+package com.bhkim.auth.entity.jpa;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -106614763L;
+
+    public static final QUser user = new QUser("user");
+
+    public final NumberPath<Integer> age = createNumber("age", Integer.class);
+
+    public final StringPath id = createString("id");
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath phoneNumber = createString("phoneNumber");
+
+    public final EnumPath<com.bhkim.auth.common.RoleEnum> role = createEnum("role", com.bhkim.auth.common.RoleEnum.class);
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public final EnumPath<com.bhkim.auth.common.TypeEnum> sex = createEnum("sex", com.bhkim.auth.common.TypeEnum.class);
+
+    public final EnumPath<com.bhkim.auth.common.TypeEnum> status = createEnum("status", com.bhkim.auth.common.TypeEnum.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+

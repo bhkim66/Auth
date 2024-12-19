a530e0bd4a030684ddad12af0e0a9f11ca827fa5
bhkim66 <bhmklnj63@gmail.com>
2024-12-19 16:32:19 +0900

    build gradle 수정

--- src/main/java/com/bhkim/auth/AuthApplication.java
+++ src/main/java/com/bhkim/auth/AuthApplication.java
@@ -3,17 +3,11 @@ package com.bhkim.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.Optional;
 
 //@ComponentScan(basePackageClasses = )
 @SpringBootApplication(scanBasePackageClasses = {BasePackage.class})
 @ConfigurationPropertiesScan({"com.bhkim.auth.properties"})
-public class AuthApplication extends SpringBootServletInitializer {
+public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}

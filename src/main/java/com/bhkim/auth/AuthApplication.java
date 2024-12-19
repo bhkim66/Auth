63f7e028a88a8135dabbb99d082b601d8b11b2fc
bhkim66 <bhmklnj63@gmail.com>
2024-12-19 15:19:58 +0900

    ServletInitializer 상속

--- src/main/java/com/bhkim/auth/AuthApplication.java
+++ src/main/java/com/bhkim/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.bhkim.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,7 +13,7 @@ import java.util.Optional;
 //@ComponentScan(basePackageClasses = )
 @SpringBootApplication(scanBasePackageClasses = {BasePackage.class})
 @ConfigurationPropertiesScan({"com.bhkim.auth.properties"})
-public class AuthApplication {
+public class AuthApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
